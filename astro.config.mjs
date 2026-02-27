45522774646a37abe2fc0a943c4ac981a15f66ab
medoismail <i.medoisma3il@gmail.com>
2026-02-27 19:49:30 +0200

    Upgrade to Astro 5, fix serverless runtime

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 
 export default defineConfig({
   site: "https://miwallpapers.vercel.app",
-  output: "hybrid",
+  output: "static",
   adapter: vercel(),
   integrations: [sitemap()],
   build: {
